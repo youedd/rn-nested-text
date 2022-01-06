ffff5b4964389bcde99b264d772d018e9058835d
youedd <eddibili.youssef@gmail.com>
2022-01-06 21:44:31 +0100

    Add snapshots tests

--- __tests__/NestedText.test.tsx
+++ __tests__/NestedText.test.tsx
@@ -6,3 +6,50 @@ it('should render simple text', () => {
   const { toJSON } = render(<NestedText>This is a Test</NestedText>)
   expect(toJSON()).toMatchSnapshot()
 })
+
+it('should render simple text with props', () => {
+  const onPressMock = jest.fn()
+  const { toJSON } = render(
+    <NestedText
+      testID='text'
+      style={{ color: 'red' }}
+      onPress={() => onPressMock()}
+    >
+      This is a Test
+    </NestedText>
+  )
+
+  expect(toJSON()).toMatchSnapshot()
+})
+
+it('should render nested text ', () => {
+  const { toJSON } = render(
+    <NestedText>{'This <b>is a <a>Link</a></b>'}</NestedText>
+  )
+
+  expect(toJSON()).toMatchSnapshot()
+})
+
+it('should render nested text with props', () => {
+  const onPressMock = jest.fn()
+  const { toJSON } = render(
+    <NestedText
+      testID='text'
+      style={{ color: 'red' }}
+      textProps={{
+        b: {
+          testID: 'bold'
+        },
+        a: {
+          testID: 'link',
+          style: { color: 'blue', textDecorationLine: 'underline' },
+          onPress: () => onPressMock()
+        }
+      }}
+    >
+      {'This <b>is a <a>Link</a></b>'}
+    </NestedText>
+  )
+
+  expect(toJSON()).toMatchSnapshot()
+})
